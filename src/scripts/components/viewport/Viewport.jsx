c00aebb042fdf1dea22e6c8214b548f56106a3d2
John Holland <thomas.holland.john@gmail.com>
2016-08-27 09:51:39 -0400

    successfully added bubbles to the scene

--- src/scripts/components/viewport/Viewport.jsx
+++ src/scripts/components/viewport/Viewport.jsx
@@ -6,6 +6,77 @@ var OrthoCamera = require('./cameras/OrthoCamera.jsx');
 
 var raycaster = new T.Raycaster();
 
+Array.matrix = function (m, n, initial)
+{
+    var a, i, j, mat = [];
+    for (i = 0; i < m; i += 1)
+    {
+        a = [];
+        for (j = 0; j < n; j += 1)
+        {
+            a[j] = initial;
+        }
+        mat[i] = a;
+    }
+    return mat;
+};
+
+function addCubes(scene)
+{
+    var columns = 9;
+    var rows = 9;
+
+    var board = Array.matrix(columns, rows, 0);
+
+    board.columns = function ()
+    {
+        return this.length;
+    };
+
+    board.rows = function ()
+    {
+        return this[0].length;
+    };
+
+    var cubeColors = ["rgb(255,0,0)", "rgb(255,255,0)", "rgb(255,0,255)",
+        "rgb(0,255,0)", "rgb(255,125,0)", "rgb(0,125,255)"
+    ];
+
+    for (var column = 0; column < board.columns() ; column++)
+    {
+        for (var row = 0; row < board.rows() ; row++)
+        {
+
+            var colorChoice = cubeColors[Math.floor(Math.random() * cubeColors.length)];
+
+            // create a cube
+            //var cubeGeometry = new THREE.BoxGeometry(4, 4, 4);
+            var cubeGeometry = new T.SphereGeometry(2.3, 20, 20);
+
+            var cubeMaterial = new T.MeshLambertMaterial({
+                color: colorChoice
+            });
+            var cube = new T.Mesh(cubeGeometry, cubeMaterial);
+
+            // position the cube
+            cube.position.x = (5 * column) - 11;
+            cube.position.y = (5 * row);
+            cube.position.z = 0;
+            cube.IsPopped = false;
+            cube.Identifier = column + "" + row;
+            cube.column = column;
+            cube.row = row;
+            cube.color = colorChoice;
+
+            board[column][row] = cube;
+
+            // add the cube to the scene
+            scene.add(cube);
+        }
+    }
+}
+
+
 var Viewport = React.createClass({
     getAspect: function ()
     {
@@ -20,6 +91,12 @@ var Viewport = React.createClass({
         return <OrbitCamera name="camera" fov={75} aspect={self.getAspect()} near={0.1} far={1000}
                             ref={_.set.bind(self, self, 'camera')} />;
     },
+
+    componentDidMount: function ()
+    {
+        addCubes(this.scene);
+    },
+
     render: function ()
     {
         var self = this;
